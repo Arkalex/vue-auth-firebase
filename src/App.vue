d2cd4e6795466f0b1a34fac49188e0edbac7585f
Alex <ayllon12@gmail.com>
2020-07-28 13:13:36 +0200

    Styles in navigation buttons

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container mt-2" id="app">
     <div>
-      <router-link to="/" v-if="existeUsuario">Inicio</router-link> |
-      <router-link to="/registro" v-if="!existeUsuario">Registro</router-link> |
-      <router-link to="/acceso" v-if="!existeUsuario">Login</router-link>
-      <button @click="cerrarSesion" v-if="existeUsuario">Logout</button>
+      <router-link to="/" v-if="existeUsuario" class="btn btn-dark mr-2">Inicio</router-link>
+      <router-link to="/registro" v-if="!existeUsuario" class="btn btn-dark mr-2">Registro</router-link>
+      <router-link to="/acceso" v-if="!existeUsuario" class="btn btn-dark mr-2">Login</router-link>
+      <button @click="cerrarSesion" v-if="existeUsuario" class="btn btn-dark mr-2">Logout</button>
     </div>
     <router-view/>
   </div>
